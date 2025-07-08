925be123081ead598fc93052004e51159272c4e6
Naoto <potepote.macrock@gmail.com>
2025-07-08 21:22:58 +0900

    fix: Fix ekyc_applications_controller_test due to routing changes (ルーティング変更に伴うekyc_applications_controller_testの修正)

--- test/controllers/ekyc_applications_controller_test.rb
+++ test/controllers/ekyc_applications_controller_test.rb
@@ -1,13 +1,26 @@
 require "test_helper"
 
 class EkycApplicationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one) # テストユーザーをセットアップ
+    sign_in @user # ログインさせる
+  end
+
   test "should get new" do
-    get ekyc_applications_new_url
+    get new_ekyc_application_url
     assert_response :success
   end
 
-  test "should get create" do
-    get ekyc_applications_create_url
-    assert_response :success
+  test "should create ekyc_application" do
+    assert_difference('EkycApplication.count') do
+      post ekyc_applications_url, params: { ekyc_application: {
+        document_type: "運転免許証",
+        document_number: "1234567890",
+        full_name: "テスト太郎",
+        date_of_birth: "2000-01-01",
+        address: "東京都"
+      } }
+    end
+    assert_redirected_to profile_url # 成功したらprofile_urlにリダイレクトされることを確認
   end
 end
